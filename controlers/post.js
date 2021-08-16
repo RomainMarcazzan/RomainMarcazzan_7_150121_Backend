82eccc7c8995ddc1faead04360d598b5c2a07342
RomainMarcazzan <romain.marcazzan@hotmail.com>
2021-08-16 08:33:45 +0400

    debbuging

--- controlers/post.js
+++ controlers/post.js
@@ -3,13 +3,14 @@ const fs = require("fs");
 
 exports.createPost = (req, res, next) => {
   const postObject = req.body;
-
+  console.log(postObject);
   const post = new Post({
     ...postObject,
     imageUrl: `${req.protocol}://${req.get("host")}/images/${
       req.file.filename
     }`,
   });
+  console.log(post);
   post
     .save()
     .then((post) => res.status(201).json(post))
